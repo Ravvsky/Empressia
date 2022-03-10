fb6cf9c06a4a7031af48d858999ca7b77f0fa236
Ravvsky <you@example.com>
2022-03-10 18:12:25 +0100

    styled-components has been appled in Tagline

--- src/components/tagline/Tagline.js
+++ src/components/tagline/Tagline.js
@@ -1,5 +1,32 @@
-import classes from "./tagline.module.css";
 import { useParallax } from "react-scroll-parallax";
+import styled from 'styled-components'
+
+const Taglines = styled.section`
+h2{font-family: 'Kelson Sans';
+font-weight: 600;
+font-size: 96px;
+line-height: 112px;
+white-space: nowrap;
+text-transform: uppercase;
+color: #000;}
+
+:nth-child(1){
+  margin-bottom: 0px;
+}
+
+> * {
+  &:nth-child(1) {
+    margin-bottom: 0px;
+  }
+
+  &:nth-child(2){
+  margin-top: 0px;
+}
+}
+
+
+`
+
 const Tagline = () => {
   const text =
     "You dream it. We plan it. — You dream it. We plan it. — You dream it. We plan it.";
@@ -12,11 +39,11 @@ const Tagline = () => {
      })
 
   return (
-      <section>
-                    <h2 className={`${classes.text} ${classes.first}`} ref={textToLeft.ref}>{text}</h2>
-                    <h2 className={`${classes.text} ${classes.second}`} ref={textToRight.ref}>{text}</h2>
+      <Taglines>
+                    <h2 ref={textToLeft.ref}>{text}</h2>
+                    <h2 ref={textToRight.ref}>{text}</h2>
 
-      </section>
+      </Taglines>
 
   );
 };
