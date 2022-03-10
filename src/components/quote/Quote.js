b837439cc97ff7c5aef89f747701a655cc382f21
Ravvsky <you@example.com>
2022-03-10 17:57:38 +0100

    styled-components has been appled in Quote

--- src/components/quote/Quote.js
+++ src/components/quote/Quote.js
@@ -1,28 +1,62 @@
-import classes from "./quote.module.css";
-import {useParallax } from "react-scroll-parallax";
+import { useParallax } from "react-scroll-parallax";
 import { useSelector } from "react-redux";
-const Quote = () => {
 
+import styled from "styled-components";
+
+const Container = styled.section`
+  position: relative;
+  text-align: center;
+  color: white;
+  height: 600px;
+  margin-bottom: 100px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const BackgroundWrapper = styled.div`
+  overflow: hidden;
+  height: 600px;
+`;
+
+const QuoteWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-family: "Kelson Sans";
+  font-weight: 600;
+  font-size: 48px;
+  line-height: 58px;
+  max-width: 900px;
+
+  p {max-width: 630px}
+`;
+
+const Quote = () => {
   const parallax = useParallax({
     scale: [1, 1.5],
-  })
+  });
 
   const language = useSelector((state) => state.language.language);
-  const quoteData = useSelector((state)=>state.quote)
+  const quoteData = useSelector((state) => state.quote);
 
   return (
-    <section className={classes.container}>
-      <div className={classes.wraper }>
+    <Container>
+      <BackgroundWrapper>
         <img
           src={quoteData.background}
           alt="background"
           ref={parallax.ref}
         ></img>
-      </div>
-      <div className={classes.quote}>
+      </BackgroundWrapper>
+      <QuoteWrapper>
         <p>{quoteData.text[language]}</p>
-      </div>
-    </section>
+      </QuoteWrapper>
+    </Container>
   );
 };
 
